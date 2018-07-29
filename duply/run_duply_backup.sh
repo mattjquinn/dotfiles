111cf9f758a9b2782317f7968f14379080c5ef4a
Matt Quinn <matt@mattjquinn.com>
2018-07-29 11:05:00 +0300

    Disabling duply.

--- duply/run_duply_backup.sh
+++ duply/run_duply_backup.sh
@@ -9,21 +9,22 @@
 # which allows it to be picked up by anacron. Ensure that this script
 # has execute permissions and belongs to root:root.
 
-logger "[MQUINN] Starting script execution..."
-
-# Is amazon.com reachable?
-/usr/bin/wget -q --tries=3 --timeout=10 -O - http://www.amazon.com > /dev/null
-wget_ret=$?
-
-logger "[MQUINN] wget return status code: $wget_ret"
-
-# If so, run backup.
-if [ "$wget_ret" == 0 ]; then
-  logger "[MQUINN] System is online; executing duply..."
-  /usr/bin/duply oracularS3 backup_purge --force
-  logger "[MQUINN] Duply execution complete."
-else
-  logger "[MQUINN] System is offline; duply will not be called."
-fi
-
-logger "[MQUINN] Script execution has finished."
+# MQUINN 02-26-2018 : Commenting out for now.
+#logger "[MQUINN] Starting script execution..."
+#
+## Is amazon.com reachable?
+#/usr/bin/wget -q --tries=3 --timeout=10 -O - http://www.amazon.com > /dev/null
+#wget_ret=$?
+#
+#logger "[MQUINN] wget return status code: $wget_ret"
+#
+## If so, run backup.
+#if [ "$wget_ret" == 0 ]; then
+#  logger "[MQUINN] System is online; executing duply..."
+#  /usr/bin/duply oracularS3 backup_purge --force
+#  logger "[MQUINN] Duply execution complete."
+#else
+#  logger "[MQUINN] System is offline; duply will not be called."
+#fi
+#
+#logger "[MQUINN] Script execution has finished."
